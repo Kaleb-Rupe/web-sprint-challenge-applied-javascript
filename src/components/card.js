35ebb46f9407000166228da3e25f1685fbeaa76e
Kaleb-Rupe <57547999+Kaleb-Rupe@users.noreply.github.com>
2023-01-21 01:33:57 -0500

    I found the issue with the code and fixed it. Now it passes the codegrade and is showing on the local server correctly.

--- src/components/card.js
+++ src/components/card.js
@@ -1,33 +1,60 @@
+import axios from "axios";
+
+// const entryPoint = document.querySelector(".cards-container");
+
 const Card = (article) => {
-  // TASK 5
-  // ---------------------
-  // Implement this function, which should return the markup you see below.
-  // It takes as its only argument an "article" object with `headline`, `authorPhoto` and `authorName` properties.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
-  //
-  // <div class="card">
-  //   <div class="headline">{ headline }</div>
-  //   <div class="author">
-  //     <div class="img-container">
-  //       <img src={ authorPhoto }>
-  //     </div>
-  //     <span>By { authorName }</span>
-  //   </div>
-  // </div>
-  //
-}
+  const card = document.createElement("div");
+  const headline = document.createElement("div");
+  const author = document.createElement("div");
+  const imgContainer = document.createElement("div");
+  const authorPhoto = document.createElement("img");
+  const authorName = document.createElement("span");
+
+  card.classList.add("card");
+  headline.classList.add("headline");
+  author.classList.add("author");
+  imgContainer.classList.add("img-container");
+
+  card.appendChild(headline);
+  card.appendChild(author);
+  author.appendChild(imgContainer);
+  imgContainer.appendChild(authorPhoto);
+  author.appendChild(authorName);
+
+  headline.textContent = article.headline;
+  authorPhoto.src = article.authorPhoto;
+  authorName.textContent = `By ${article.authorName}`;
+
+  card.addEventListener("click", () => {
+    console.log(headline.textContent);
+  });
+
+  return card;
+};
 
 const cardAppender = (selector) => {
-  // TASK 6
-  // ---------------------
-  // Implement this function that takes a css selector as its only argument.
-  // It should obtain articles from this endpoint: `http://localhost:5001/api/articles` (test it with console.log!!).
-  // However, the articles do not come organized in a single, neat array. Inspect the response closely!
-  // Create a card from each and every article object in the response, using the Card component.
-  // Append each card to the element in the DOM that matches the selector passed to the function.
-  //
-}
+  axios
+    .get("http://localhost:5001/api/articles")
+    .then((resp) => {
+      resp.data.articles.javascript.forEach((cardInfo) => {
+        document.querySelector(selector).appendChild(Card(cardInfo));
+      });
+      resp.data.articles.bootstrap.forEach((cardInfo) => {
+        document.querySelector(selector).appendChild(Card(cardInfo));
+      });
+      resp.data.articles.jquery.forEach((cardInfo) => {
+        document.querySelector(selector).appendChild(Card(cardInfo));
+      });
+      resp.data.articles.technology.forEach((cardInfo) => {
+        document.querySelector(selector).appendChild(Card(cardInfo));
+      });
+      resp.data.articles.node.forEach((cardInfo) => {
+        document.querySelector(selector).appendChild(Card(cardInfo));
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-export { Card, cardAppender }
+export { Card, cardAppender };
